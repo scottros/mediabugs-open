b09c64e8afc7c460b7260d18422ced84fec0854f
benbrown <benbrown@8fce6638-e741-f231-da20-5b075d9b3f1e>
2011-01-26 17:27:06 +0000

    =Updated bug browser now controlled by flexible plugins

--- trunk/pods/bugs_browse/index.php
+++ trunk/pods/bugs_browse/index.php
@@ -17,25 +17,115 @@
 
 	$mediabugs_account = $POD->anonymousAccount();
 
+	include_once("browser.php");
 
+	// load all potential browse modes.  
+	$dir = dirname(__FILE__).'/browsers/';
+    if ($dh = opendir($dir)) {
+        while (($file = readdir($dh)) !== false) {
+        	if (!is_dir($file) && preg_match("/\.php/",$file)) {
+	        	include_once($dir . $file);
+	       	} 		
+        }
+        closedir($dh);
+    }
+    
 	$offset = 0;
 	if (isset($_GET['offset'])) {
 		$offset = $_GET['offset'];
 	}
 
 	$mode = @$_GET['mode'] ? $_GET['mode'] : 'home';
-	$sort = $_GET['sort'] ? $_GET['sort'] : 'date';
+	$sort = @$_GET['sort'] ? $_GET['sort'] : 'date';
 
 	$crumbs = array();
-	$crumbs[] = "<a href=\"/bugs\">Browse Bugs</a>";
+	$crumbs[] = "<a href=\"" . $POD->siteRoot(false) . "/bugs\">Browse Bugs</a>";
 	$feed = null;
+	
+	$B = new Browser($POD,$crumbs);
+
 	if ($mode !='home') { 
-		$feed = "/bugs/feeds/" .  $mode  ."?q=". urlencode($_GET['q']);
+		$feed = "/bugs/feeds/" .  $mode  ."?q=". urlencode(@$_GET['q']);
 		$POD->header("Bugs by $mode",$feed);
 	} else {
 		$POD->header('Search MediaBugs');
 	}
+
+	echo '<div class="column_8">';
+
+		if ($mode=='home') {
+			
+			// display the browse homepage with all the various browse options
+		
+			echo "<h1>Browse Bugs</h1>";
+			echo "<ul class=\"directory\">";
+			foreach ($B->modelist() as $mode=>$stuff) {
+				$B->browseStarters($mode);
+			}
+			echo "</ul>";
+		} else {
 	
+			if (@$_GET['q']) { 
+				// do media outlet search
+				if ($mode=='search') {
+					$term = $_GET['q'];
+					$outlets = $POD->getContents(array('headline:like'=>"%{$term}%",'type'=>'bug_target'));
+	
+				}
+				if ($outlets && $outlets->count() >0) { 
+					$outlets->output('did_you_mean');
+				}
+				// print out the results
+				$B->browseBy($mode,$_GET['q'],$sort,$offset);
+			} else {
+				// either print results, or print a secondary level of browse options
+				$B->browseDefault($mode,$sort,$offset);
+			}
+			
+		}
+	echo '</div>';
+
+	$subscribed = false;
+	if ($POD->isAuthenticated() && @$_GET['q']) { 
+	
+		$subs = $POD->getContents(array('userId'=>$POD->currentUser()->id,'type'=>'subscription','query_type'=>$mode,'body'=>$_GET['q']));
+		$subscribed = ($subs->totalCount() > 0);
+	}
+
+	
+	?>
+		<div class="column_4 last">
+			<? if (@$_GET['q'] || $mode=='date') { ?>
+
+				<div class="sidebar">
+					<h3>Subscribe</h3>
+					<p><a href="/bugs/feeds/<?= $mode ?>?q=<?= urlencode(@$_GET['q']); ?>">Subscribe to these results via RSS</a> to receive new bugs in your feed reader.</p>
+					<? if ($POD->isAuthenticated() && @$_GET['q'] && ($mode=='status' || $mode=='date' || $mode=='outlet'||$mode=='type' ||$mode=='search')) { ?>
+					
+						<p><?= $POD->toggleBot($subscribed,'togglesub','Stop receiving updates','E-mail me updates','method=toggleSub&keyword='.urlencode($_GET['q'])."&type=".urlencode($mode)); ?>
+							whenever a bug is added to this list.
+						</p>
+					<? } ?>
+				</div>
+			<? } ?>
+			
+			<? $POD->output('sidebars/recent_bugs'); ?>
+		
+			<? $POD->output('sidebars/browse'); ?>
+		
+		</div>	
+	<?
+	
+	$POD->footer();
+
+	exit;
+	
+
+
+	// EVERYTHING ELSE IS DEPRECATED!!
+
+
+
 	if ($mode == "list" || ($mode=="outlet" && !isset($_GET['q']))) {
 
 		$crumbs[] = "<a href=\"/bugs/browse/outlet\">Media Outlets</a>";
